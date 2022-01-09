83a47d88b12735277fa2206c64d03e21015da49c
amyeguo <72362221+amyeguo@users.noreply.github.com>
2022-01-09 13:40:02 -0600

    Modularize code

--- src/App.js
+++ src/App.js
@@ -1,90 +1,40 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
-const terms = { F: "Fall", W: "Winter", S: "Spring" };
+import CourseList from "./components/CourseList";
 
-const getCourseTerm = (course) => terms[course.id.charAt(0)];
+const meetsPat =
+  /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
 
-const getCourseNumber = (course) => course.id.slice(1, 4);
-
-const toggle = (x, lst) =>
-  lst.includes(x) ? lst.filter((y) => y !== x) : [x, ...lst];
-
-const Course = ({ course, selected, setSelected }) => {
-  const isSelected = selected.includes(course);
-  const style = {
-    backgroundColor: isSelected ? "lightgreen" : "white",
-  };
-  return (
-    <div
-      className="card m-1 p-2"
-      style={style}
-      onClick={() => setSelected(toggle(course, selected))}
-    >
-      <div className="card-body">
-        <div className="card-title">
-          {getCourseTerm(course)} CS {getCourseNumber(course)}
-        </div>
-        <div className="card-text">{course.title}</div>
-        <div className="card-text">{course.meets}</div>
-      </div>
-    </div>
-  );
+const timeParts = (meets) => {
+  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+  return !match
+    ? {}
+    : {
+        days,
+        hours: {
+          start: hh1 * 60 + mm1 * 1,
+          end: hh2 * 60 + mm2 * 1,
+        },
+      };
 };
+
+const mapValues = (fn, obj) =>
+  Object.fromEntries(
+    Object.entries(obj).map(([key, value]) => [key, fn(value)])
+  );
+
+const addCourseTimes = (course) => ({
+  ...course,
+  ...timeParts(course.meets),
+});
+
+const addScheduleTimes = (schedule) => ({
+  title: schedule.title,
+  courses: mapValues(addCourseTimes, schedule.courses),
+});
+
 const Banner = ({ title }) => <h1>{title}</h1>;
-
-const TermButton = ({ term, setTerm, checked }) => (
-  <>
-    <input
-      type="radio"
-      id={term}
-      className="btn-check"
-      checked={checked}
-      autoComplete="off"
-      onChange={() => setTerm(term)}
-    />
-    <label class="btn btn-success m-1 p-2" htmlFor={term}>
-      {term}
-    </label>
-  </>
-);
-
-const TermSelector = ({ term, setTerm }) => (
-  <div className="btn-group">
-    {Object.values(terms).map((value) => (
-      <TermButton
-        key={value}
-        term={value}
-        setTerm={setTerm}
-        checked={value === term}
-      />
-    ))}
-  </div>
-);
-
-const CourseList = ({ courses }) => {
-  const [term, setTerm] = useState("Fall");
-  const [selected, setSelected] = useState([]);
-  const termCourses = Object.values(courses).filter(
-    (course) => term === getCourseTerm(course)
-  );
-
-  return (
-    <>
-      <TermSelector term={term} setTerm={setTerm} />
-      <div className="course-list">
-        {termCourses.map((course) => (
-          <Course
-            key={course.id}
-            course={course}
-            selected={selected}
-            setSelected={setSelected}
-          />
-        ))}
-      </div>
-    </>
-  );
-};
 
 const App = () => {
   const [schedule, setSchedule] = useState();
@@ -95,7 +45,7 @@ const App = () => {
       const response = await fetch(url);
       if (!response.ok) throw response;
       const json = await response.json();
-      setSchedule(json);
+      setSchedule(addScheduleTimes(json));
     };
     fetchSchedule();
   }, []);
